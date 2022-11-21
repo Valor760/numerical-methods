2db03a60c21801f095ff11cfc0561712a8b360da
stanislavsn1 <stanislavsn1@verifone.com>
2022-11-21 13:11:34 +0200

    Implemented LSQ, but probably not working correctly

--- LSA.py
+++ LSA.py
@@ -1,5 +1,33 @@
 from math import sqrt
 import time
+import matplotlib.pyplot as plt
+
+def solve_lsq(X: list, Y:list, n: int, k: int) -> list:
+	# Fill matrix A
+	matA = [[1] * k]
+	matA[0][0] = n
+	# for power in range(1, k):
+	# 	buff = []
+	# 	for num in X:
+	# 		buff.append(num ** power)
+	# 	matA[0].append(sum(buff))
+	for power in range(1, k):
+		buff = []
+		for num in X:
+			buff.append(num ** power)
+		matA.append([1] * k)
+		matA[power][power] = sum(buff)
+	
+	# Fill matrix B
+	matB = [sum(Y)]
+	for power in range(1, k):
+		buff = []
+		for idx in range(n):
+			buff.append((X[idx] ** power) * Y[idx])
+		matB.append(sum(buff))
+
+	solve_gauss_elim(matA, matB, n)
+	return matB
 
 
 def solve_gauss_elim(A: list, B: list, n: int) -> list:
@@ -10,7 +38,7 @@ def solve_gauss_elim(A: list, B: list, n: int) -> list:
 		nums = []
 		for j in range(i, n): # j - row
 			nums.append(abs(A[j][i])) # Add absolute value of each row in column
-		lead_row_idx = nums.index(max(nums)) + i
+		lead_row_idx = nums.index(max(nums))
 
 		# Place leading row on the ith place (don't forget to swap in B vec)
 		A[i], A[lead_row_idx] = A[lead_row_idx], A[i] 
@@ -54,29 +82,32 @@ def split_and_convert_to_float(data: str) -> list:
 
 
 def main() -> None:
-	vecA = split_and_convert_to_float(input("Input A vector(space separated):\n"))
-	
-	# Check vector A is NxN size
-	n = sqrt(len(vecA))
-	if n != round(n):
-		print("Matrix is not NxN size!")
-		print(f"Your matrix: [{vecA}]")
-		return
-	n = int(n) # sqrt returns float...
-	matA = convert_to_matrix(vecA, n)
+	size_n = int(input("Input size N: "))
 
-	vecB = split_and_convert_to_float(input("Input B vector(space separated):\n"))
-	if len(vecB) != n:
-		print("Vector B length is not equal to A matrix length!")
-		print(f"Vector B length: {len(vecB)}   Matrix A length: {n}")
+	vecX = split_and_convert_to_float(input("Input vector X(space separated):\n"))
+	if len(vecX) != size_n:
+		print(f"ERROR: vector X size != N: X length - {len(vecX)} | N - {size_n}")
+		return
+
+	vecY = split_and_convert_to_float(input("Input vector Y(space separated):\n"))
+	if len(vecY) != size_n:
+		print(f"ERROR: vector Y size != N: Y length - {len(vecY)} | N - {size_n}")
+		return
+
+	k = int(input("Input K order: "))
+	if k < 0:
+		print(f"ERROR: Order K couldn't be less than 0! Your value = {k}")
 		return
 
 	# Solve equation
 	start = time.time()
-	solve_gauss_elim(matA, vecB, n)
+	result = solve_lsq(vecX, vecY, size_n, k)
 	duration = time.time() - start
 
-	print(f"\nAnswer is: {[round(x, 4) for x in vecB]}")
+	plt.plot(result)
+	plt.show()
+
+	print(f"\nAnswer is: {[round(x, 4) for x in result]}")
 	print(f"Time: {round(duration * 1000, 2)}ms")	
 
 
