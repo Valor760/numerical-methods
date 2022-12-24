bc602ed9fb6d9e48b97c8b9a86285407ef10be67
valor760 <valorcool@gmail.com>
2022-12-24 15:38:32 +0200

    Eulers method fully working

--- eulers.py
+++ eulers.py
@@ -1,17 +1,18 @@
 from math import exp
 import matplotlib.pyplot as plt
 
-def func_analytic(a:float, b: float, x0:float, t: float) -> float:
-	return (a * x0 * exp(a * t)) / (a + b * x0 * (exp(a * t) - 1))
+def func_analytic(a: float, b: float, x0: float, t: float) -> float:
+	return (a * x0 * exp(a * t)) / (a + (b * x0 * (exp(a * t) - 1)))
+	# return (2.66667 * exp(0.8 * t)) / (1.66667 + exp(0.8 * t))
 
 
-def func(a: float, b: float, x0: float) -> float:
-	return a * x0 - b * (x0 ** 2)
+def func(a: float, b: float, x: float) -> float:
+	return a * x - b * (x ** 2)
 
 
 def solve_eulers(a: float, b: float, h: float, x0: float, r_end: float):
-	t = [0]
-	t0 = 0 + h
+	t = []
+	t0 = 0
 
 	# Prepare t array
 	while t0 <= r_end:
@@ -23,8 +24,8 @@ def solve_eulers(a: float, b: float, h: float, x0: float, r_end: float):
 	arr_given = [x0]
 	arr_error = [0]
 	for i in range(1, len(t)):
-		arr_analytic.append(arr_analytic[i-1] + h * func_analytic(a, b, arr_analytic[i-1], t[i]))
-		arr_given.append(arr_given[i-1] + h * func(a, b, arr_given[i-1]))
+		arr_analytic.append(func_analytic(a, b, x0, t[i]))
+		arr_given.append(arr_given[-1] + h * func(a, b, arr_given[-1]))
 		arr_error.append(abs(arr_given[i] - arr_analytic[i]))
 
 	return arr_given, arr_analytic, arr_error, t
@@ -39,8 +40,11 @@ def main() -> None:
 
 	arr_given, arr_analytic, arr_error, t = solve_eulers(a, b, h, x0, r_end)
 
-	plt.plot(t, arr_analytic, 'b')
 	plt.plot(t, arr_given, 'r')
+	plt.plot(t, arr_analytic, 'b', linestyle='dotted')
+	plt.show()
+
+	plt.plot(t, arr_error)
 	plt.show()
 
 if __name__ == "__main__":
